fd9aab301a93423bd9acfca9fe15bafd4364eba3
alenreuel <reuel.alen@gmail.com>
2024-08-17 16:52:05 +0800

    Fixed a few typos

--- run_bot.py
+++ run_bot.py
@@ -1,11 +1,28 @@
 import reddit_bot
+import time
+import requests
 
-x = reddit_bot.RedditBot()
 
 
+# check if internet is operational 
+
+
+# outer-loop: checks for internet connection
+# inner-loop: first logs in and the bot runs forever/ until the user does 
+# not want it to run :) 
 while True:
+    random_url = "https://www.reddit.com/"
+    timeout = 10
     try:
-        x.check_mentions()
-    except:
-        continue
+        # requesting URL
+        request = requests.get(random_url,timeout=timeout)
+        # if runs through without issues, run main code block
+        x = reddit_bot.RedditAIBot()
+        while True:
+            x.check_mentions()
+            time.sleep(30) 
+ 
+    # catching exception for when internet is not running
+    except (requests.ConnectionError,requests.Timeout) as exception:
+        time.sleep(300)
     
